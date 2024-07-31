31c56a9464b094546f5c635b4d525ec6d6c991e3
su111yeon <su111yeon@gmail.com>
2024-08-01 00:00:05 +0900

    compostion API

---
+++ .eslintrc.cjs
@@ -0,0 +1,30 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-prettier/skip-formatting'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'prettier/pretitier': [
+      'error',
+      {
+        singleQuote: true,
+        semi: true,
+        useTabs: true,
+        trailngComma: 'all',
+        printWidth: 80,
+        bracketSpacing: true,
+        arrowParens: 'avoid'
+      }
+    ]
+  }
+}
---
+++ src/lectures/4/AppCompostionAPI.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <h2>반응형 메시지</h2>
+    <p>{{ reactiveMessage }}</p>
+    <button v-on:click="addReactiveMesssage">Add message</button>
+    <h2>일반 메시지</h2>
+    <p>{{ normalMessage }}</p>
+    <button v-on:click="addNormalMesssage">Add message</button>
+  </div>
+</template>
+
+<script>
+import { isRef, onMounted, onBeforeMount, onUpdated, ref } from 'vue'
+
+export default {
+  setup() {
+    console.log('setup()')
+    // 반응형 상태 선언
+    const reactiveMessage = ref('Reactive Message')
+    // 일반 변수 선언
+    let normalMessage = 'Normal Message'
+
+    console.log('isRef(reactiveMessage): ', isRef(reactiveMessage)) // true
+    console.log('isRef(normalMessage): ', isRef(normalMessage)) // false
+
+    const addReactiveMesssage = () => {
+      reactiveMessage.value = reactiveMessage.value + '!'
+    }
+    const addNormalMesssage = () => {
+      normalMessage = normalMessage + '!'
+    }
+
+    onMounted(() => {
+      console.log('onMounted')
+    })
+
+    onBeforeMount(() => {
+      console.log('onBeforeMount')
+    })
+
+    onUpdated(() => {
+      console.log('update component')
+    })
+
+    return {
+      reactiveMessage,
+      normalMessage,
+      addReactiveMesssage,
+      addNormalMesssage
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
