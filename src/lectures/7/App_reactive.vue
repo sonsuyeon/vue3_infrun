67d25c0df3996dbe8cc55617d59a2fe849a744b4
su111yeon <su111yeon@gmail.com>
2024-08-01 23:28:09 +0900

    vue setup ~ 반응형기초

---
+++ src/lectures/7/App_reactive.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <button v-on:click="incrment">Click {{ state.count }}</button>
+    <button v-on:click="incrment">Deep Click {{ state.deep.count }}</button>
+  </div>
+</template>
+
+<script>
+/* 반응형 기초 */
+import { reactive } from 'vue'
+export default {
+  setup() {
+    const state = reactive({
+      count: 0,
+      deep: {
+        count: 0
+      }
+    })
+    const incrment = () => {
+      state.count++
+      state.deep.count++
+    }
+
+    return {
+      state,
+      incrment
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/lectures/7/App_ref.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <p>{{ message }}</p>
+    <button v-on:click="addMessage">add Click</button>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+  setup() {
+    let message = ref('Hello Vue!')
+
+    const addMessage = () => {
+      message.value = message.value + '!'
+    }
+    console.log('messtage : ', message.value)
+    console.log('messtage typeof :', typeof message.value)
+    return {
+      message,
+      addMessage
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
